a17031536235543071ef8652e84e8237b4f15568
GautierHub <45164608+gotogotleroux@users.noreply.github.com>
2021-07-06 13:39:55 +0200

    gsap reveal animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,3 @@
-// counter script - gsap animated numbers
-
-const items = document.querySelectorAll(".counter");
-
-gsap.from(items, {
-  textContent: 0,
-  duration: 2,
-  ease: "power3",
-  snap: { textContent: 1 },
-  stagger: {
-    each: 1.0,
-    onUpdate: function () {
-      this.targets()[0].innerHTML = this.targets()[0].textContent + "+";
-    },
-  },
-});
-
 // navbar script - gsap animated numbers
 
 $(function () {
@@ -24,6 +7,71 @@ $(function () {
     } else {
       $(".navbar").removeClass("active");
     }
+  });
+});
+
+// reveal script - gsap animated reveal & counter animation
+
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+    y = direction * 100;
+  if (elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    x = 100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_counter")) {
+    y = 0;
+    gsap.from(elem, {
+      x: 0,
+      y: 0,
+      textContent: 0,
+      duration: 2,
+      ease: "power3",
+      snap: { textContent: 1 },
+      stagger: {
+        each: 1.0,
+        onUpdate: function () {
+          this.targets()[0].innerHTML = this.targets()[0].textContent + "+";
+        },
+      },
+    });
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(
+    elem,
+    { x: x, y: y, autoAlpha: 0 },
+    {
+      duration: 2,
+      x: 0,
+      y: 0,
+      autoAlpha: 1,
+      ease: "expo",
+      overwrite: "auto",
+    }
+  );
+}
+
+function hide(elem) {
+  gsap.set(elem, { autoAlpha: 0 });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  gsap.registerPlugin(ScrollTrigger);
+
+  gsap.utils.toArray(".gs_reveal").forEach(function (elem) {
+    hide(elem);
+
+    ScrollTrigger.create({
+      trigger: elem,
+      onEnter: function () {
+        animateFrom(elem);
+      },
+      onLeaveBack: (self) => self.disable(),
+    });
   });
 });
 
